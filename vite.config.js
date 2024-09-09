21d9b1b0876e0b2df95fadf547d4af62428c51b8
Mingyang <141198127+MingyangXiaKira@users.noreply.github.com>
2024-09-09 19:21:44 +0100

    dependency modify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["@mui/icons-material"], // 添加这行
+    },
+  },
 });
