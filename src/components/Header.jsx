57e97ed0a7f6bad187097b3691a7a72bada67867
Mingyang <141198127+MingyangXiaKira@users.noreply.github.com>
2024-09-11 14:32:52 +0100

    add section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 const Header = () => {
   return (
-    <header className="bg-blue-900 text-white p-3 mb-5">
-      <h1 className="text-2xl text-center font-bold uppercase">
-        React Playground
-      </h1>
-    </header>
+    <a href="/">
+      <header className="bg-blue-900 text-white p-3 mb-5">
+        <h1 className="text-2xl text-center font-bold uppercase">
+          React Playground
+        </h1>
+      </header>
+    </a>
   );
 };
 export default Header;
